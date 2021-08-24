4110560746d15f431824a85b07063a8ae1f0f2f8
qunzhongcheng@creditease.cn <qunzhongcheng@creditease.cn>
2021-08-24 10:24:01 +0800

    提交代码

---
+++ packages/alert/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="vf-alert" role="alert">
+    <div class="vf-alert__content">
+      <div class="vf-alert__title">
+        <slot name="title">{{ title }}</slot>
+      </div>
+      <p class="vf-alert__description" v-if="$slots.default && !description">
+        <slot></slot>
+      </p>
+      <p class="vf-alert__description" v-if="description && !$slots.default">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "VfAlert",
+  data() {
+    return {
+      visible: true,
+    };
+  },
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    description: String,
+  },
+});
+</script>
---
+++ packages/theme/README.md
@@ -0,0 +1,35 @@
+# VF-UI theme
+
+VF-UI component theme
+
+## Installation
+
+```shell
+npm install --save vfui-theme
+```
+
+## Usage
+
+Use Less import
+
+```less
+@import "vfui-theme";
+```
+
+Or Use webpack
+
+```js
+import "vfui-theme";
+```
+
+Or Use In UMD
+
+```html
+<link rel="stylesheet" path="path/to/node_modules/vfui-theme/lib/index.css" />
+```
+
+## Import on demand
+
+```js
+import "vfui-theme/lib/input.css";
+```
